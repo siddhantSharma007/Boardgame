f3abfe56d20c3b10ed44d391346e024f372e1cc4
Siddhant Sharma <102786233+siddhantSharma007@users.noreply.github.com>
2024-04-04 14:08:02 +0530

    Update and rename deploy_docker_image.yml to ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  
+  instance_type = "t2.micro"              
+  key_name      = "cicd"            
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
