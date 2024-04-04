a55a45b2152e31c53311687b35b3b58f6dbfb2af
Siddhant Sharma <102786233+siddhantSharma007@users.noreply.github.com>
2024-04-04 15:19:53 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -43,15 +43,43 @@ resource "aws_route" "internet_gateway_route" {
   gateway_id             = aws_internet_gateway.my_igw.id
 }
 
+# Create Security Group
+resource "aws_security_group" "my_sg" {
+  name        = "MySecurityGroup"
+  description = "Allow inbound traffic on port 80 and 22"
+  vpc_id      = aws_vpc.my_vpc.id
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EC2 instance
 resource "aws_instance" "web_ec2_instance" {
-  ami                    = "ami-007020fd9c84e18c7"  # Replace with your desired AMI ID for web tier
+  ami                    = "ami-09298640a92b2d12c"  # Replace with your desired AMI ID for web tier
   key_name               = "cicd"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.subnet_a.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
   
   tags = {
     Name = "WebEC2Instance"
   }
 }
-
-
