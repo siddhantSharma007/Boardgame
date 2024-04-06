f3f65c27a800fee30378cf77b0725079131be124
Siddhant Sharma <102786233+siddhantSharma007@users.noreply.github.com>
2024-04-06 14:30:31 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -57,7 +57,14 @@ resource "aws_security_group" "my_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+ ingress {
+    from_port   = 8080  # Allow traffic on port 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 22
     to_port     = 22
